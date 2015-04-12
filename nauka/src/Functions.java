cf0b2870d4e8bff3fc050fee2577716c4437ccbb
Paulina <paulina.uszko@gmail.com>
2015-04-12 20:51:08 +0200

    commit nowy

--- nauka/src/Functions.java
+++ nauka/src/Functions.java
@@ -1,28 +1,45 @@
 
-public class Functions {
- 
-	class Student {
-	    private String firstName;
-	    private String lastName;
-	    public Student(String firstName, String lastName) {
-	        this.firstName = firstName;
-	        this.lastName = lastName;}
-	    public void printFullName(){
-	        System.out.println(firstName + lastName);}
-	}
 
-	public class Main {
-	    public void main(String[] args) {
-	        Student[] students = new Student[] {
-	            new Student("Morgan", "Freeman"),
-	            new Student("Brad", "Pitt"),
-	            new Student("Kevin", "Spacey"),
-	        };
-	        for (Student s : students) {
-	            s.printFullName();
-	        }
-	    }
-	}
+public class Functions{
+		
+	    	    public double area ()
+	    	    {
+	    	        return 0;    }
+
+	    	    public static void main(String[] args) {
+	    	    	Functions s1 = new Circle (2.5);
+	    	    	Functions s2 = new Rectangle (5.0, 4.0);
+	    	        
+	    	        System.out.println (s1.area());
+	    	        System.out.println (s2.area());
+	    	    }
+	    	}
 
 
-}
+class Circle extends Functions {                    // class declaration
+	    	    Circle (double diameter) {                  // constructor
+	    	        this.diameter = diameter;
+	    	    }
+	    	    private static final double PI = Math.PI;   // constant
+	    	    private double diameter;                    // instance variable
+	    	    
+	    	    public double area () {                     // dynamic method
+	    	        double radius = diameter / 2.0;
+	    	        return PI * radius * radius;
+	    	    }
+
+	    	}
+
+
+class Rectangle extends Functions {
+				private double b;
+				private double a;
+
+				Rectangle(double a, double b){
+					this.a = a; this.b = b;}
+				public double area () {
+					return a*b;
+				}}
+
+
+
